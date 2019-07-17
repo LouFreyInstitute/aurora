80d55d2e2fc6cddd737a5a69364577ea79296902
Mike Barnhardt <mikebarnhardt+github@gmail.com>
2019-07-17 11:29:59 -0400

    Update 404 page content.

--- packages/aurora-documentation/src/pages/404.js
+++ packages/aurora-documentation/src/pages/404.js
@@ -5,8 +5,11 @@ import SEO from '../components/seo';
 
 const NotFoundPage = () => (
 	<Layout>
-		<SEO title="404: Not found" />
-		<h1>NOT FOUND</h1>
+		<SEO title="404 Page Not Found" />
+
+		<h1 className="c-heading is-large">
+			Oops! This page doesn&apos;t exist&hellip;
+		</h1>
 		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
 	</Layout>
 );
