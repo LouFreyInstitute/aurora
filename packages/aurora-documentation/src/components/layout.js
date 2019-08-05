96ce1757a0cc6f65c2f0821ebd541e0a9efef398
Mike Barnhardt <mikebarnhardt+github@gmail.com>
2019-08-05 12:26:07 -0400

    Add working localStorage theme and dark mode.

--- packages/aurora-documentation/src/components/layout.js
+++ packages/aurora-documentation/src/components/layout.js
@@ -1,3 +1,5 @@
+/* global window */
+
 /**
  * Layout component that queries for data
  * with Gatsby's useStaticQuery component
@@ -5,73 +7,119 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {useStaticQuery, graphql} from 'gatsby';
+import {graphql, StaticQuery} from 'gatsby';
 
 import ThemeContext from './theme-context';
 import Header from './header';
 import Sidebar from './sidebar';
 
-const Layout = ({children, size}) => {
-	const data = useStaticQuery(graphql`
-		query SiteTitleQuery {
-			site {
-				siteMetadata {
-					title
+class Layout extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			theme: 'Aurora',
+			dark: false
+		};
+
+		this.setTheme = this.setTheme.bind(this);
+		this.setDark = this.setDark.bind(this);
+	}
+
+	setTheme(theme) {
+		this.setState({theme});
+
+		if (typeof window !== 'undefined') {
+			window.localStorage.setItem('theme', theme);
+		}
+	}
+
+	setDark() {
+		this.setState(
+			previousState => ({dark: !previousState.dark}),
+			() => {
+				if (typeof window !== 'undefined') {
+					window.localStorage.setItem('dark', this.state.dark);
 				}
 			}
+		);
+	}
+
+	componentDidMount() {
+		if (typeof window !== 'undefined') {
+			const theme = window.localStorage.getItem('theme');
+			const dark = window.localStorage.getItem('dark') === 'true';
+			this.setState({theme, dark});
 		}
-	`);
+	}
 
-	const [theme, setTheme] = useState('Aurora');
-	const [dark, setDark] = useState(false);
-	const toggleDark = () => setDark(prev => !prev);
+	render() {
+		const {children, size} = this.props;
+		const {theme, dark} = this.state;
 
-	return (
-		<ThemeContext.Provider value={theme}>
-			<div
-				className={`t-${theme.toLowerCase()} has-overrides ${
-					dark ? 'is-dark' : ''
-				} o-layout is-vertical u-full-height`}
-			>
-				<Header
-					changeMode={toggleDark}
-					changeTheme={setTheme}
-					dark={dark}
-					siteTitle={data.site.siteMetadata.title}
-					theme={theme}
-				/>
+		return (
+			<StaticQuery
+				query={graphql`
+					query SiteTitleQuery {
+						site {
+							siteMetadata {
+								title
+							}
+						}
+					}
+				`}
+				render={data => (
+					<ThemeContext.Provider value={theme}>
+						<div
+							className={`t-${theme.toLowerCase()} has-overrides ${
+								dark ? 'is-dark' : ''
+							} o-layout is-vertical u-full-height`}
+						>
+							<Header
+								changeMode={this.setDark}
+								changeTheme={this.setTheme}
+								dark={dark}
+								siteTitle={data.site.siteMetadata.title}
+								theme={theme}
+							/>
 
-				<div className="o-layout is-horizontal o-layout-item is-fill">
-					<Sidebar />
+							<div className="o-layout is-horizontal o-layout-item is-fill">
+								<Sidebar />
 
-					<main className="o-block is-large o-layout is-vertical o-layout-item is-fill">
-						<div className="o-layout-item is-fill u-margin-bottom+ s-prose">
-							<div className={`o-container is-${size}`}>{children}</div>
-						</div>
+								<main className="o-block is-large o-layout is-vertical o-layout-item is-fill">
+									<div className="o-layout-item is-fill u-margin-bottom+ s-prose">
+										<div className={`o-container is-${size}`}>{children}</div>
+									</div>
 
-						<hr className="divider" />
+									<hr className="divider" />
 
-						<footer className="u-font-size- s-prose">
-							<div className="o-layout">
-								<div>
-									© {new Date().getFullYear()},{' '}
-									<a href="http://loufreyinstitute.org">Lou Frey Institute</a>{' '}
-									at the University of Central Florida.
-								</div>
-								<div className="o-layout-item is-fill" />
-								<div>
-									Built with <a href="https://www.gatsbyjs.org">Gatsby</a>.
-								</div>
+									<footer className="u-font-size- s-prose">
+										<div className="o-layout">
+											<div>
+												© {new Date().getFullYear()},{' '}
+												<a href="http://loufreyinstitute.org">
+													Lou Frey Institute
+												</a>{' '}
+												at the University of Central Florida.
+											</div>
+											<div className="o-layout-item is-fill" />
+											<div>
+												Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+												.
+											</div>
+										</div>
+									</footer>
+								</main>
 							</div>
-						</footer>
-					</main>
-				</div>
-			</div>
-		</ThemeContext.Provider>
-	);
-};
+						</div>
+					</ThemeContext.Provider>
+				)}
+			/>
+		);
+	}
+}
 
 Layout.propTypes = {
 	children: PropTypes.node.isRequired
